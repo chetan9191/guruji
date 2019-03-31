69e4b574041347a507abdf909c774f88acecc71c
chetan9191 <47293049+chetan9191@users.noreply.github.com>
2019-03-31 09:25:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//API KEY = 7ef4e9a783cfb36f54d482b2e28f44a7 
-
-$.getJSON("https://www.metaweather.com/api/location/search/?query=london",function(api){
-console.log(api);
+$.getJSON("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7ef4e9a783cfb36f54d482b2e28f44a7",function(data){
+	
+console.log(data);
 
 });
