71b499a6a38d1fcb011be72898fbd077d6c31122
chetan9191 <47293049+chetan9191@users.noreply.github.com>
2019-03-31 09:29:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//API KEY = 7ef4e9a783cfb36f54d482b2e28f44a7 
+
+$.getJSON("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7ef4e9a783cfb36f54d482b2e28f44a7",function(data){
+	
+console.log(data);
+
+});
